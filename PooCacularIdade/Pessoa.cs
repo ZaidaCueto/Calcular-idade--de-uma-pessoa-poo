4aabd7e87bf63baccbe6f956d777cf355ec63e3a
ZaidaCueto <zcueto58@gmail.com>
2023-01-15 21:19:31 -0300

    Adicionar arquivos de projeto.

---
+++ PooCacularIdade/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooCacularIdade
+{
+  public class Pessoa
+    {
+        //digitar propfull para criar estrutura  base para set get de manira mais facil
+        private int anoNacimento;
+
+		public int AnoNacimento	
+		{
+			get { return this.anoNacimento; }
+			set { this.anoNacimento = value; }
+		}
+
+		private string nome;
+   
+
+        public string Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+		public void ExibriDados()
+		{
+
+			Console.WriteLine();
+			Console.WriteLine($"nome: {this.Nome}");
+			Console.WriteLine($"Ano de nacimento: {this.AnoNacimento}");
+			int idade = this.CalcularIdade();
+			Console.WriteLine($"Idade: {idade} ");
+		}
+	private int CalcularIdade()
+		{
+			DateTime data = DateTime.Now;
+			int ano = data.Year;
+			int idade = ano - this.AnoNacimento;
+            return idade;
+		}
+	}
+}
---
+++ PooCacularIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooCacularIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Programa calcula idade de uma pessoa");
+            Pessoa p = new Pessoa();
+            Console.Write("Nome da pessoa: ");
+            p.Nome = Console.ReadLine(); 
+            Console.Write("Ano de nascimento: ");
+            p.AnoNacimento = int.Parse(Console.ReadLine());
+            p.ExibriDados();
+            Console.Clear();
+        }
+    }
+}
